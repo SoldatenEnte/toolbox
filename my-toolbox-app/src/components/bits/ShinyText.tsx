1f10a485f3b0cd6fcbe1f7496b0ec7c1db5f1cb4
SoldatenEnte <ostehof.weertzen@gmail.com>
2025-10-05 23:05:03 +0200

    Update ShinyText.tsx

--- my-toolbox-app/src/components/bits/ShinyText.tsx
+++ my-toolbox-app/src/components/bits/ShinyText.tsx
@@ -20,7 +20,7 @@ const ShinyText: React.FC<ShinyTextProps> = ({ text, disabled = false, speed = 5
             )}
             style={{
                 backgroundImage:
-                    'linear-gradient(60deg, var(--color-muted-foreground) 10%, white 50%, var(--color-muted-foreground) 70%)',
+                    'linear-gradient(120deg, white 10%, var(--color-muted-foreground) 50%, white 70%)',
                 backgroundSize: '200% 100%',
                 WebkitBackgroundClip: 'text',
                 animationDuration,
